b9e3572c548edf7fe760a09dabac51275accf193
Dávid Szabó <sasasoft3hun@windowslive.com>
2017-05-16 12:01:17 +0200

    UpdateViewState in Transfer and Attack View.
    Accept for Transfer and Attack in Controller.

--- project/RiskGameJava/src/Controller/RiskGameController.java
+++ project/RiskGameJava/src/Controller/RiskGameController.java
@@ -79,8 +79,7 @@ public class RiskGameController extends java.util.Observable implements java.uti
     		case Battle:
     			if (previouslySelectedTerritory != -1)
     			{
-					//if (model.checkAttackPossible(territoryID, previouslySelectedTerritory))
-    				if (false)
+					if (model.checkAttackPossible(territoryID, previouslySelectedTerritory))
 					{
 						addLog("Attacking " + country.getName() + "!");
 						showAttackView(territoryID, previouslySelectedTerritory);
@@ -95,8 +94,7 @@ public class RiskGameController extends java.util.Observable implements java.uti
     		case Transfer:
     			if (previouslySelectedTerritory != -1)
     			{
-					//if (model.checkTransferPossible(previouslySelectedTerritory, territoryID))
-    				if (false)
+					if (model.checkTransferPossible(previouslySelectedTerritory, territoryID))
 					{
 						addLog("Transferring units to " + country.getName() + "!");
 						showTransferView(previouslySelectedTerritory, territoryID);
@@ -163,6 +161,23 @@ public class RiskGameController extends java.util.Observable implements java.uti
 		attackView.AddControllerListener(this);
 		//attackView.UpdateViewState(defender, attacker);
 	}
+	
+	public void attackAccepted(int defender, int attacker, int defenderUnits, int attackerUnits) {
+		Country defenderCountry = convertToCountry(defender);
+		Country attackerCountry = convertToCountry(attacker);
+		
+		try {
+			if (model.attackTerritory(defender, attacker, defenderUnits, attackerUnits))
+			{
+				addLog(attackerCountry.getName() + " attacked " + defenderCountry.getName() + "!");
+				//attackView.UpdateViewState(attackResult);
+			}
+			else
+				addLog(attackerCountry.getName() + " failed to attack " + defenderCountry.getName() + "!");
+		} catch (Exception e) {
+			addLog(e.getMessage());
+		}
+	}
 
 	/*
 	 * 	Transfer View
@@ -171,6 +186,23 @@ public class RiskGameController extends java.util.Observable implements java.uti
 		transferView = new View.JFXTransferView();
 		transferView.AddControllerListener(this);
 		//transferView.UpdateViewState(from, to);
+	}
+	
+	public void transferAccepted(int from, int to, int units) {
+		Country fromCountry = convertToCountry(from);
+		Country toCountry = convertToCountry(to);
+		
+		try {
+			//if (model.transfer(from, to, units))
+			if (false) {
+				addLog(units + " transferred from " + fromCountry.getName() + " to " + toCountry.getName() + "!");
+				//transferView.dieSomehow(); //TODO: Hide transfer window
+			}
+			else
+				addLog("Failed to transfer " + units + " units from " + fromCountry.getName() + " to " + toCountry.getName() + "!");
+		} catch (Exception e) {
+			addLog(e.getMessage());
+		}
 	}
 
 	/*
@@ -191,17 +223,19 @@ public class RiskGameController extends java.util.Observable implements java.uti
 	 * 	Pre -> Main
 	 */
 	public void switchToMain(Map<String, Color> map){
-		preStage.hide();
+		//preStage.hide();
 		try {
-			model.addPlayerToPlayerList(map);
-
 			System.out.println(map.toString());
+			model.addPlayerToPlayerList(map);
+			
+			preStage.close();
+			showMainView();
 		} catch (Exception e) {
-			System.err.println("oh boy");
-			e.printStackTrace();
+			//System.err.println("oh boy");
+			//e.printStackTrace();
+			addLog(e.getMessage());
 		}
-		preStage.close();
-		showMainView();
+
 	}
 	
 	/**
@@ -218,7 +252,7 @@ public class RiskGameController extends java.util.Observable implements java.uti
 	
 	//A fõablak naplójába küld egy új bejegyzést
 	public void addLog(String log) {
-		
+		System.out.println(log);
 	}
 	
 	
--- project/RiskGameJava/src/View/JFXTransferView.java
+++ project/RiskGameJava/src/View/JFXTransferView.java
@@ -1,6 +1,15 @@
 package View;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import Model.Territory;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.AmbientLight;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 
 /**
  * Ezen a View-en valaszthatja ki a jatekos, hogy a sajat teruleten belul hova
@@ -10,6 +19,15 @@ import Model.Territory;
  * @created 19-ápr.-2017 23:11:49
  */
 public class JFXTransferView extends JFXViewBase {
+	
+	@FXML private Label lblPlayerName;
+	@FXML private Label lblFromCountryName;
+	@FXML private Label lblFromQuantity;
+	@FXML private Label lblToCountryName;
+	@FXML private Label lblToQuantity;
+	
+	@FXML private Slider sldSoilderQuantity;
+	@FXML private Button btnAccept;
 
 	public JFXTransferView(){
 		LoadFXML("/View/fxml/TransferView.fxml");
@@ -19,6 +37,19 @@ public class JFXTransferView extends JFXViewBase {
 	public void finalize() throws Throwable {
 		super.finalize();
 	}
+	
+	@Override
+    public void initialize(URL location, ResourceBundle resources) {
+		
+		btnAccept.setOnMouseClicked(evt -> {
+			int from = Integer.parseInt(lblFromCountryName.getText());
+			int to = Integer.parseInt(lblToCountryName.getText());
+			Double amount = sldSoilderQuantity.getValue();
+			
+			controller.transferAccepted(from, to, amount.intValue());
+		});
+		
+	}
 
 	/**
 	 * 
@@ -26,7 +57,13 @@ public class JFXTransferView extends JFXViewBase {
 	 * @param to
 	 */
 	public void UpdateViewState(Territory from, Territory to){
-
+		//lblPlayerName.setText(from.getOwner());
+		
+		lblFromCountryName.setText(Country.values()[from.getId()].toString());
+		lblToCountryName.setText(Country.values()[to.getId()].toString());
+		
+		lblFromQuantity.setText(Integer.toString(from.getUnits()));
+		lblToQuantity.setText(Integer.toString(to.getUnits()));
 	}
 
 }
